cc47e8111d8a2aee1fa58de94c47dfd295acc976
mukun_poudel <mukun@herm.is>
2023-09-18 12:10:33 +0530

    Remove un authenticate flow

--- webviews/components/UnAuthenticated.svelte
+++ webviews/components/UnAuthenticated.svelte
@@ -31,9 +31,9 @@
   async function authenticateWithGitHub() {
     vscodeChannel.postMessage({ type: constKeys.onAuthenticate, value: undefined });
   }
-  async function unAuthenticateWithGitHub() {
-    vscodeChannel.postMessage({ type: constKeys.unAuthenticate, value: undefined });
-  }
+  // async function unAuthenticateWithGitHub() {
+  //   vscodeChannel.postMessage({ type: constKeys.unAuthenticate, value: undefined });
+  // }
 </script>
 
 <main class="unauthenticated">
@@ -41,7 +41,7 @@
   <b>Looks like you are not authenticated</b>
   <p>Zist requires you to authenticate your VS Code with Github to access and create snippets</p>
   <button on:click={() => authenticateWithGitHub()}>Authenticate with GitHub</button>
-  <button on:click={() => unAuthenticateWithGitHub()}> UnAuthenticate with GitHub</button>
+  <!-- <button on:click={() => unAuthenticateWithGitHub()}> UnAuthenticate with GitHub</button> -->
   <h6>*When you authenticate with GitHub you are authenticating VS Code not Zist itself.</h6>
   <h5>Note: If you are having trouble authenticating, please try `Ctrl + Shift + P` and look for 'Zist Authenticate' command</h5>
 </main>
